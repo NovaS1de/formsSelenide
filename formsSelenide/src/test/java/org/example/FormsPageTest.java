46554787fbc29ef89641bff27a935fe13118b2ab
NovaS1de <148364873+NovaS1de@users.noreply.github.com>
2024-07-03 13:55:23 +0400

    Add files via upload

--- formsSelenide/src/test/java/org/example/FormsPageTest.java
+++ formsSelenide/src/test/java/org/example/FormsPageTest.java
@@ -15,7 +15,7 @@ public class FormsPageTest extends MainTest{
     }
     @Test
     public void testingForm(){
-        homePage.open().clickFormsButton().clickPracticeFormButton().fillFirstName("Tommy").fillLastName("Rexis").fillMobileNumber("0000000000").clickGenderMaleButton().clickSubmitButton().fillEmail("kek@kek.kek");
+        homePage.open().clickFormsButton().clickPracticeFormButton().fillFirstName("Tommy").fillLastName("Rexis").fillMobileNumber("0000000000").clickGenderMaleButton().fillEmail("kek@kek.kek").clickSubmitButton();
         Assert.assertTrue(formsPage.getSubmitFormPopup().isDisplayed());
         sleep(5000);
     }
